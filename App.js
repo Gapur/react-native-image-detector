5ee1ba55176d999f92989a6b3b995f6ba37deec5
Gapur <gapur.kassym@gmail.com>
2022-10-13 23:08:30 +0600

    feat(app): update screen styles

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Camera, useCameraDevices} from 'react-native-vision-camera';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  ActivityIndicator,
+} from 'react-native';
+import { Camera, useCameraDevices } from 'react-native-vision-camera';
 
-const App = () => {
+export default function App() {
   const [cameraPermission, setCameraPermission] = useState();
+  const [detetorValue, setDetectorValue] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -12,31 +19,78 @@ const App = () => {
     })();
   }, []);
 
-  console.log(`Re-rendering Navigator. Camera: ${cameraPermission}`);
+  console.log(`Camera permission status: ${cameraPermission}`);
 
   const devices = useCameraDevices();
-  const device = devices.back;
+  const cameraDevice = devices.back;
+
+  const renderDetectorContent = () => {
+    if (cameraDevice && cameraPermission === 'authorized') {
+      return (
+        <Camera style={styles.camera} device={cameraDevice} isActive={true} />
+      );
+    }
+    return <ActivityIndicator size="large" color="#1C6758" />;
+  };
 
   return (
-    <View style={styles.container}>
-      {device && cameraPermission === 'authorized' ? (
-        <Camera style={styles.camera} device={device} isActive={true} />
-      ) : null}
+    <View style={styles.screen}>
+      <SafeAreaView style={styles.saveArea}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>React Native Image Detector</Text>
+        </View>
+      </SafeAreaView>
+
+      <View style={styles.caption}>
+        <Text style={styles.captionText}>
+          Welcome To React-Native-Vision-Camera Tutorial
+        </Text>
+      </View>
+
+      {renderDetectorContent()}
+
+      <Text style={styles.detectorValueText}>{detetorValue}</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
+    backgroundColor: '#EEF2E6',
+  },
+  saveArea: {
+    backgroundColor: '#3D8361',
+  },
+  header: {
+    height: 50,
+    backgroundColor: '#3D8361',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'black',
+  },
+  headerText: {
+    color: '#ffffff',
+    fontSize: 20,
+  },
+  caption: {
+    height: 120,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captionText: {
+    color: '#100F0F',
+    fontSize: 16,
+    fontWeight: '600',
   },
   camera: {
-    flex: 1,
-    width: '100%',
+    height: 540,
+    width: '92%',
+    alignSelf: 'center',
+  },
+  detectorValueText: {
+    paddingVertical: 20,
+    textAlign: 'center',
+    color: '#100F0F',
+    fontSize: 24,
   },
 });
-
-export default App;
